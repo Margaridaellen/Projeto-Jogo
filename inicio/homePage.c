9f81103009bc5772c92a0d0eb58efbac39c41f90
FranciscoGabriel <franciscogabriel1220@gmail.com>
2026-09-10 19:41:30 -0300

    initial project

--- inicio/homePage.c
+++ inicio/homePage.c
@@ -2,8 +2,24 @@
 #include <raylib.h>
 #include <stdlib.h>
 
-void main(){
+#define larguraTela 1920
+#define alturaTela 1080
+
+int main(){
+ InitWindow(larguraTela,alturaTela,"inicio");
+ SetTargetFPS(60);
+
+ while(!WindowShouldClose()){
 
 
-    
+
+    BeginDrawing();
+    ClearBackground(BLUE);
+
+        EndDrawing();
+ }
+
+CloseWindow();
+
+    return 0;
 }
