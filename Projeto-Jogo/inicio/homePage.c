3dbda1bfb725eb56358261403dea24d52aebfa0d
Margaridaellen <margarida.gomes@alunos.ufersa.edu.br>
2026-09-11 11:10:06 -0300

    Implementação das setas

--- Projeto-Jogo/inicio/homePage.c
+++ Projeto-Jogo/inicio/homePage.c
@@ -1,25 +1,55 @@
 #include <stdio.h>
-#include "raylib.h"
+#include <raylib.h>
 #include <stdlib.h>
+
 #define AZUL_ESCURO (Color){10, 25, 38, 255}
 #define larguraTela 1920
 #define alturaTela 1080
+#define movimento 10.0f
 
-int main()
-{
-    InitWindow(larguraTela, alturaTela, "inicio");
-    SetTargetFPS(60);
+int main(void){
 
-    while (!WindowShouldClose())
-    {
+ InitWindow(larguraTela,alturaTela,"inicio");
+ SetTargetFPS(60);
 
-        BeginDrawing();
-        ClearBackground(AZUL_ESCURO);
+ //Posições iniciais
+    float pos1=400.0f;
+    float pos2=300.0f;
+    float r=25.0f;
 
-        EndDrawing();
+//Velocida em pixels
+    float v_inicial=300.0f;
+
+while (!WindowShouldClose()){
+//Calcular o deltatime(dt)
+    float dt = GetFrameTime();
+    float v = v_inicial*dt;
+
+    if(IsKeyDown(KEY_RIGHT)){
+        pos1 = pos1 + v;  //direita
+    }
+    if(IsKeyDown(KEY_LEFT)){
+        pos1 = pos1 - v; //esquerda
+    }
+    if(IsKeyDown(KEY_DOWN)){
+        pos2 = pos2  +v; //para baixo
+    }
+    if(IsKeyDown(KEY_UP)){
+        pos2= pos2-v; // para cima
     }
 
-    CloseWindow();
+    BeginDrawing(); //O que deve aparecer na tela
+    ClearBackground(AZUL_ESCURO);
+
+        //Desenhar circulo (pos1 e pos2 são parâmetos que definem o centro do circulo e o r controla o tamanho)
+        DrawCircle( (int) pos1 , (int) pos2 , r , BLACK);
+
+        //Escreve texto na tela, 10-distância da borda esquerda até o inicio do texto, 10-Topo da janela até o topo do texto, 20-Tamanho da fonte 
+        DrawText("Utilize as setas do teclado para se movimentar", 10, 10 , 20, BLACK);
+    EndDrawing();
+ }
+
+CloseWindow();
 
     return 0;
 }
